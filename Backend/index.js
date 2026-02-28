0dcedaab5c8b2d6f04dd438f6cb2e76539d310b8
abhinayvanaparthi <vanaparthyaabhinay@gmail.com>
2026-02-28 18:58:39 +0530

    Initial backend setup with Express and Nodemon

---
+++ Backend/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+const PORT = 5000;
+
+// Middleware
+app.use(express.json());
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("Messaging backend is running 🚀");
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
