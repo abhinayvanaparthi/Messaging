e967f24db0527e71cdf9c2d6954c81506fe14a29
abhinayvanaparthi <vanaparthyaabhinay@gmail.com>
2026-02-28 21:59:12 +0530

    jwc and auth api implementation.

---
+++ Backend/controllers/authController.js
@@ -0,0 +1,39 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+// Signup
+exports.registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    // Check if user exists
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    // Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Create user
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    // Generate token
+    const token = jwt.sign({ id: user._id }, "secretkey", {
+      expiresIn: "7d",
+    });
+
+    res.status(201).json({
+      message: "User registered",
+      token,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
--- Backend/index.js
+++ Backend/index.js
@@ -1,17 +1,20 @@
+require("dotenv").config();
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+const authRoutes = require("./routes/authRoutes");
 
-// Middleware
+connectDB();
+
 app.use(express.json());
+app.use("/api/auth", authRoutes);
 
-// Test route
 app.get("/", (req, res) => {
   res.send("Messaging backend is running 🚀");
 });
 
-// Start server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
