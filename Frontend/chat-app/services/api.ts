40d156e03c77d89bbd4ec4f5599dcc3a1adee507
abhinayvanaparthi <vanaparthyaabhinay@gmail.com>
2026-03-14 22:45:46 +0530

    removed extra /api added in axios create.

--- Frontend/chat-app/services/api.ts
+++ Frontend/chat-app/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL + "/api",
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
 });
 
 // attach token automatically
